b9ae51106e5df0430f267c17f0274b7deb745033
iotcat <i@iotcat.me>
2019-06-28 10:30:11 +0800

    add days to cookie

--- fp.js
+++ fp.js
@@ -6,8 +6,8 @@
  */
 
 var cookie = {
-  set: function (name, value) {
-    var Days = 3000;
+  set: function (name, value, Days) {
+    if(Days == undefined) var Days = 3000;
     var exp = new Date();
     exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
     document.cookie = name + '=' + escape(value) + ';expires=' + exp.toGMTString() + ";path=/";
